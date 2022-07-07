d289d9977267f964f3b02c6420b0bde469698fd2
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-07-07 14:34:29 -0700

    commit tree 2bb697934e53b76bfc892038f0cff53836b0c62a

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import Todo from "./Todo";
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+  
+  const todos = useSelector(state => state.todos.entities);
+  console.log(todos)
+
+  return (
+  <ul>
+    {todos.map(todo => <Todo key={todo} todo={todo}/>)}
+  </ul>
+  )
 }
 
 export default TodoContainer;
